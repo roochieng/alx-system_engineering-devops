fed03af866a221d9729e0e924bb44e2f3c0ac95a
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-25 20:15:01 +0000

    Corrections

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,11 +4,13 @@ import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     def to_do_list(user_id):
-        user_id = sys.argv[1]
+
         url = "https://jsonplaceholder.typicode.com/"
-        user = requests.get(url + f"users/{user_id.json()}"
+        user = requests.get(url + "users/{}".format(user_id)).json()
         username = user.get("username")
         todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
@@ -17,4 +19,4 @@ if __name__ == "__main__":
             [writer.writerow(
                 [user_id, username, t.get("completed"), t.get("title")]
             ) for t in todos]
-to_do_list(user_id)
+    to_do_list(user_id)
