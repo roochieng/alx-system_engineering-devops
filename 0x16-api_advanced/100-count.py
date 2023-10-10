3e9b6303d3b1882e45d509d45f070570f182c58b
roochieng <49204011+roochieng@users.noreply.github.com>
2023-10-10 19:42:34 +0000

    API advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -53,6 +53,6 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
             print("")
             return
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print(f"{k}: {v}" for k, v in instances]
+        print(f"{k}: {v}" for k, v in instances)
     else:
         count_words(subreddit, word_list, instances, after, count)
