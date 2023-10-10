e1f464138780a802d1c75842bf602f3ffaf9b7bf
roochieng <49204011+roochieng@users.noreply.github.com>
2023-10-10 19:31:58 +0000

    API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and
+returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
